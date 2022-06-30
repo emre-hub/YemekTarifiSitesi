0dd949f5b07f20d22080d173eab41808643dc2cb
Emre <74258534+emre-hub@users.noreply.github.com>
2022-06-30 13:51:06 +0300

    YemekDetay Sayfasi ve Kodlari
    YemekDetay.aspx.cs sayfasi, AnaSayfadan yönlendirilerek açılıyor, 
    sayfa yüklenirken arka planda gerçekleşen işlemleri kodda yorum satırlarıyla birlikte görebilirsiniz.

---
+++ YemekDetay.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Kullanici.Master" AutoEventWireup="true" CodeBehind="YemekDetay.aspx.cs" Inherits="Yemek_Tarifi_Sitesi_ASPNET.YemekDetay" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style10 {
+            font-size: xx-large;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <strong>
+    <asp:Label ID="Label3" runat="server" CssClass="auto-style10" Text="Label"></asp:Label>
+    </strong>
+</asp:Content>
---
+++ YemekDetay.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Yemek_Tarifi_Sitesi_ASPNET
+{
+    public partial class YemekDetay : System.Web.UI.Page
+    {
+        sqlbaglanti conn = new sqlbaglanti();
+        string yemekid = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //AnaSayfa'dan YemekDetay sayfasına taşınan değer, "YemekId".
+            //AnaSayfa'da yemek adına tıklandığında sorgu olarak YemekId'yi alıyor,
+            //sorgu(query)yu string bir degiskene atıyorum(yemekid) :
+            yemekid = Request.QueryString["YemekId"]; 
+
+            SqlCommand komut = new SqlCommand("SELECT YemekAd FROM Tbl_Yemekler WHERE YemekId=@p1", conn.baglanti());
+            komut.Parameters.AddWithValue("@p1", yemekid); //id degerini sqlcommand sorgusuna ekliyorum
+            SqlDataReader sqlDataReader = komut.ExecuteReader();
+            while (sqlDataReader.Read()) //okunacak veri unique id'ye sahip tek bir veri
+            {
+                Label3.Text = sqlDataReader[0].ToString(); //veriye ulastigimda YemekAdi olan 0. index'teki degeri String olarak Label3'e atiyorum.
+            }
+            conn.baglanti().Close();
+        }
+    }
+}
